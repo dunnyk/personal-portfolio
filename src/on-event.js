df076db99b808a2175e008783d3e31d91a662ef4
Robertson Akpan <robertsonakpan@gmail.com>
2022-05-10 11:23:41 +0100

    format navlink wrapper and add close icon event

--- src/on-event.js
+++ src/on-event.js
@@ -1,36 +1,46 @@
 // const hamburgerBtn = document.querySelector('.hamburger');
 // const navLinksWrapper = document.querySelector('.hide-mobile');
 
-
 // hamburgerBtn.addEventListener('click', () => {
 //   console.log("CLicked????????????")
 //   navLinksWrapper.style.display = 'block';
 //   navLinksWrapper.lastElementChild.style.display = 'none';
 // });
-
 
 // function displayMenu(){
 //   console.log("Cion")
 // }
 
-
-
 // hamburgerBtn.addEventListener('click', () => {
 //   console.log("CLicked????????????")
 //   navLinksWrapper.style.display = 'block';
 //   navLinksWrapper.lastElementChild.style.display = 'none';
 // });
 
+function displayMenu() {
+  function insertElement(elem, className) {
+    const element = document.createElement(elem);
+    element.className = className;
+    return element;
+  }
 
-function displayMenu(){
   const hamburgerBtn = document.querySelector('.hamburger');
   const navLinksWrapper = document.querySelector('.hide-mobile');
 
   hamburgerBtn.addEventListener('click', () => {
-    navLinksWrapper.style.display = "flex"
-  })
-  // navLinksWrapper.style.display = 'flex';
-  // navLinksWrapper.lastElementChild.style.display = 'none';
+    const cross = insertElement('img', 'cross');
+    cross.src = './image/cross.png';
+    const li = insertElement('li', 'cross-icon-wrapper');
+    li.appendChild(cross);
+
+    cross.addEventListener('click', () => {
+      navLinksWrapper.style.display = 'none';
+      li.remove();
+      cross.remove();
+    });
+    navLinksWrapper.appendChild(li);
+    navLinksWrapper.style.display = 'flex';
+  });
 }
 
-displayMenu()
+displayMenu();
