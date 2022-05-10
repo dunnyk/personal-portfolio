fc1948ac3169b5e4f67249083f0e37011f157ab6
dunnyk <danielkamau231@gmail.com>
2022-05-10 15:58:55 +0300

    Update javascript classes in on-event.js file

--- src/on-event.js
+++ src/on-event.js
@@ -1,22 +1,3 @@
-// const hamburgerBtn = document.querySelector('.hamburger');
-// const navLinksWrapper = document.querySelector('.hide-mobile');
-
-// hamburgerBtn.addEventListener('click', () => {
-//   console.log("CLicked????????????")
-//   navLinksWrapper.style.display = 'block';
-//   navLinksWrapper.lastElementChild.style.display = 'none';
-// });
-
-// function displayMenu(){
-//   console.log("Cion")
-// }
-
-// hamburgerBtn.addEventListener('click', () => {
-//   console.log("CLicked????????????")
-//   navLinksWrapper.style.display = 'block';
-//   navLinksWrapper.lastElementChild.style.display = 'none';
-// });
-
 function displayMenu() {
   function insertElement(elem, className) {
     const element = document.createElement(elem);
@@ -24,38 +5,38 @@ function displayMenu() {
     return element;
   }
 
-  const hamburgerBtn = document.querySelector('.hamburger');
-  const navLinksWrapper = document.querySelector('.hide-mobile');
-  const cross = insertElement('img', 'cross');
-  const li = insertElement('li', 'cross-icon-wrapper');
+  const hamburgerBtn = document.querySelector(".hamburger");
+  const navLinksWrapper = document.querySelector(".hide-mobile");
+  const cross = insertElement("img", "cross");
+  const li = insertElement("li", "cross-icon-wrapper");
 
   function onClickDisappear() {
-    navLinksWrapper.style.display = 'none';
+    navLinksWrapper.style.display = "none";
     li.remove();
     cross.remove();
   }
 
-  hamburgerBtn.addEventListener('click', () => {
-    cross.src = './image/cross.png';
+  hamburgerBtn.addEventListener("click", () => {
+    cross.src = "./image/cross.png";
     li.appendChild(cross);
-    cross.addEventListener('click', () => {
+    cross.addEventListener("click", () => {
       onClickDisappear();
     });
     navLinksWrapper.appendChild(li);
-    navLinksWrapper.style.display = 'flex';
+    navLinksWrapper.style.display = "flex";
   });
 
-  const portfolioNav = document.querySelector('#portfolio');
-  portfolioNav.addEventListener('click', (e) => {
+  const portfolioNav = document.querySelector("#portfolio");
+  portfolioNav.addEventListener("click", (e) => {
     onClickDisappear();
     e.stopPropagation();
   });
 
-  document.querySelector('#about').addEventListener('click', () => {
+  document.querySelector("#about").addEventListener("click", () => {
     onClickDisappear();
   });
 
-  document.querySelector('#contact').addEventListener('click', () => {
+  document.querySelector("#contact").addEventListener("click", () => {
     onClickDisappear();
   });
 }
