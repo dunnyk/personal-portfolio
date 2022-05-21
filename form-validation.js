4d9bd6d0c71071d50788a85e4cb2165cab087208
dunnyk <danielkamau231@gmail.com>
2022-05-22 00:19:32 +0300

    Update changes

--- form-validation.js
+++ form-validation.js
@@ -4,8 +4,7 @@ const small = userEmail.parentNode.querySelector('small');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (userEmail.value !== userEmail.value.toLowerCase())
-  {
+  if (userEmail.value !== userEmail.value.toLowerCase()) {
     small.innerText = 'Form not sent.Please enter email in lower case';
   }
   else if (userEmail.value === userEmail.value.toLowerCase()) small.innerText = '';
