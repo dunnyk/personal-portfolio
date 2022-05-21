7869383106f32c828d2dc9fe46aab879822edff5
dunnyk <danielkamau231@gmail.com>
2022-05-21 22:35:39 +0300

    Update Validating contact form

--- form-validation.js
+++ form-validation.js
@@ -3,8 +3,8 @@ const userEmail = document.getElementById('email');
 const small = userEmail.parentNode.querySelector('small');
 
 form.addEventListener('submit', (e) => {
+  e.preventDefault();
   if (userEmail.value !== userEmail.value.toLowerCase()) {
-    e.preventDefault();
     small.innerText = 'Form not sent.Please enter email in lower case';
   } else if (userEmail.value === userEmail.value.toLowerCase()) small.innerText = '';
 });
